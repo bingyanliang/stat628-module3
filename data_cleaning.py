4a3e85cbdc0c0976c6e16c1e7cfa00cea0d2a734
bingyanliang <115088000+bingyanliang@users.noreply.github.com>
2023-11-05 19:44:30 -0600

    add data cleaning file

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,26 +1,29 @@
 import pandas as pd
-import json
 
-chunk_size = 10000 
+# Define chunk size
+chunk_size = 100000  # Adjust this depending on your system's memory
 
-business_chunks = pd.read_json('business.json', lines=True, chunksize=chunk_size)
+# Use an iterator to load chunks
+business_iterator = pd.read_json('business.json', lines=True, chunksize=chunk_size)
 
-business_data_ca_list = []
+# Filter businesses in CA and concatenate into one DataFrame
+business_data_ca = pd.concat([chunk[chunk['state'] == 'FL'] for chunk in business_iterator])
 
-for chunk in business_chunks:
-    business_data_ca_chunk = chunk[chunk['state'] == 'CA']
-    business_data_ca_list.append(business_data_ca_chunk)
+# Save the California business data to CSV (it should be small enough)
+business_data_ca.to_csv('business_data_fl.csv', index=False)
 
-business_data_ca = pd.concat(business_data_ca_list, ignore_index=True)
+# Now read the reviews in chunks and iteratively merge with business_data_ca
+review_iterator = pd.read_json('review.json', lines=True, chunksize=chunk_size)
 
-
-review_chunks = pd.read_json('review.json', lines=True, chunksize=chunk_size)
-
-merged_data_list = []
-
-for chunk in review_chunks:
+# We'll append each filtered and merged chunk to a CSV file
+first_chunk = True  # To write headers in the first chunk only
+for chunk in review_iterator:
+    # Merge with the filtered business data
     merged_chunk = pd.merge(business_data_ca, chunk, on='business_id', how='inner')
-    merged_data_list.append(merged_chunk)
+    
+    # Append to a CSV file
+    with open('reviews_in_fl.csv', 'a', newline='', encoding='utf-8') as f:
+        merged_chunk.to_csv(f, index=False, header=first_chunk)
+        first_chunk = False  # Disable header writing for subsequent chunks
 
-merged_data = pd.concat(merged_data_list, ignore_index=True)
-
+# After the loop, 'reviews_in_ca.csv' will have the merged data of reviews for businesses in CA
